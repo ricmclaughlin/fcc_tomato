bb547b3914dc3771fa972d22644c1f81e1f80542
ric mclaughlin <ric@mclaughlin.today>
2016-10-10 20:43:36 -0700

    ux hack

--- components/Home.js
+++ components/Home.js
@@ -1,18 +1,67 @@
 import React, { Component } from 'react'
 import { transparentBg } from '../styles'
 import { Link } from 'react-router'
-
+import '../main.css'
+import '../circle.css'
 class Home extends Component {
   render () {
     return (
       <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
-        <h1>My New Fancy App</h1>
-        <p className="lead">Some Fancy Motto</p>
-        <Link to="/#">
-          <button className="btn btn-lg btn-success" type="button">Get Started</button>
-        </Link>
+        <Title />
+        <Controls />
+        <Circle />    
       </div>
     )
   }
 };
+
+function Title (){
+  return (
+    <h1>Tomato Timer</h1>
+  )
+}
+
+function Controls (){
+  return (
+    <div className="row settings">
+      <div className="col-md-3"></div>
+      <div className="col-md-3">
+        <form className="settings-control">
+          <input type='button' value='-' className='qtyminus' />
+          <input type='text' name='quantity' value='5' className='qty' />
+          <input type='button' value='+' className='qtyplus' />
+        </form>
+      </div>
+      <div className="col-md-3">
+        <form className="settings-control">
+          <input type='button' value='-' className='qtyminus' />
+          <input type='text' name='quantity' value='25' className='qty' />
+          <input type='button' value='+' className='qtyplus' />
+        </form>
+      </div>
+      <div className="col-md-3"></div>
+    </div>
+  )
+}
+
+function Circle (){
+  return (
+    <div className="row circle">
+      <div className="col-md-3"></div>
+      <div className="col-md-6">
+        <div className="c100 p50 big">
+          <span>5:00</span>
+          <div className="slice">
+              <div className="bar"></div>
+              <div className="fill"></div>
+          </div>
+        </div>
+      </div>
+      
+      <div className="col-md-3"></div>
+    </div>
+  )
+}
+
+
 export default Home
--- config/routes.js
+++ config/routes.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import Main from '../components/Main'
 import Home from "../components/Home"
 
-var routes = (
-  <Router history={hashHistory}>
+export const routes = (
+  <Router history={browserHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={Home} />
     </Route>
   </Router>
 );
-
-module.exports = routes;
